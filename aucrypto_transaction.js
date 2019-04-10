cb228dd259a84a116b80ce7b1bbb2483ef600131
liamgib <40730821+liamgib@users.noreply.github.com>
2019-04-10 13:38:55 +1000

    functionality of user database.

--- aucrypto_transaction.js
+++ aucrypto_transaction.js
@@ -13,19 +13,35 @@ app.listen(3000, async () => {
   await database.doesTableExist('users').then(exists => {
     if(!exists) {
       database.getUserDatabase().createUserTable();
-      
+    }
+  });
+
+  await database.doesTableExist('session').then(exists => {
+    if(!exists) {
+      database.getUserDatabase().createSessionTable();
     }
   });
 
   //let user_id = await database.getUserDatabase().createUser('liamagibasaa', 'testing123');
-  
+
   /*
-  database.getUserDatabase().loginUser('liamagibasaa', 'testing123').then(result => {
+  let session;
+  await database.getUserDatabase().loginUser('liamagibasaa', 'testing123').then(result => {
     let isLoggedIn = result[0]
     let session_or_verifykey = result[1];
+    session = result[1];
     console.log("A", isLoggedIn, session_or_verifykey);
   });
+  let is_session = await database.getUserDatabase().isSession(session);
+  console.log(session, is_session);
   */
+ 
+  //let verify_user = await database.getUserDatabase().userVerification('0c5596a79c0e55e953c2b3b7dd93affb0b6bb763', 6976);
+  //console.log("Verified: ", verify_user);
+
+
+  //let email_exists = await database.getUserDatabase().doesEmailExist('liamgibsaa');
+  //console.log(email_exists);
 
   console.log('AUCRYPTO - Transaction worker started → PORT 3000');
 });
--- postgres/user_database.js
+++ postgres/user_database.js
@@ -21,6 +21,15 @@ module.exports.createUserTable = () => {
     });
 }
 
+module.exports.createSessionTable = () => {
+    return promise = new Promise(function(resolve, reject) {
+        pool.query("CREATE TABLE session(session VARCHAR(45) UNIQUE NOT NULL, user_id VARCHAR(45) NULL)", (err, res) => {
+            if (err) return resolve(false);
+            return true;
+        });
+    });
+}
+
 
 //      -=- User functions -=-
 
@@ -46,6 +55,12 @@ module.exports.createUser = async (email, password) => {
       }
 }
 
+/**
+ * Used to login the user with a give email or password.
+ * @param {String} email The associated accounts email address.
+ * @param {String} password The associated accounts password.
+ * @returns {Promise} ifLoggedIn, session_or_verifykey. 
+ */
 module.exports.loginUser = async (email, password) => {
     return promise = new Promise(async function(resolve, reject) {
         const client = await pool.connect()
@@ -64,7 +79,7 @@ module.exports.loginUser = async (email, password) => {
                         resolve([false, rows[0].verifykey]);
                     }else{
                         let session = crypto.randomBytes(16).toString("hex");
-                        //TODO: CREATION SESSION TABLE AND INSERT INTO TABLE.
+                        await client.query("INSERT INTO session(session, user_id) VALUES ($1, $2)", [session, rows[0].user_id]);
                         await client.query('COMMIT');
                         resolve([true, session]);
                     }
@@ -75,12 +90,71 @@ module.exports.loginUser = async (email, password) => {
             }
         } catch (e) {
         await client.query('ROLLBACK')
-        return (false);
+        return ([false]);
         } finally {
         client.release();
         }
-    });
+    }).catch();
 }
+
+/**
+ * Verify a user account, give then verifykey and pin.
+ * @param {String} verifykey The given verifiykey for the user.
+ * @param {String} pin The pin to verify the user. 
+ * @returns {Boolean} Returns a async boolean, true or false if complete.
+ */
+module.exports.userVerification = async (verifykey, pin) => {
+    try {
+        await pool.query("UPDATE users SET verified='Y' where verifykey=$1 AND verifypin=$2", [verifykey, pin]);
+        return true;
+    } catch(e) {
+        return false;
+    }
+}
+
+
+/**
+ * Check if an email exists, if so return true or false.
+ * @param {String} email The email to search for
+ * @returns {Promise} Returns a promise that resolves
+ */
+module.exports.doesEmailExist = async (email) => {
+    try {
+    const { rows } = await pool.query('SELECT user_id from users where email=$1', [email]);
+        if (rows[0] === undefined || rows.length == 0 || rows == null) {
+            return false;
+        } else {
+            return true;
+        }
+    } catch(e) {
+        return false;
+    }
+}
+
+/**
+ * Checks if a session is valid, returns true if so.
+ * @param {String} session The session to check.
+ * @returns {Booleaan} Returns a async boolean as true or false.
+ */
+module.exports.isSession = async (session) => {
+    try {
+        let { rows } = await pool.query('SELECT user_id from session where session=$1', [session]);
+        let user_id = rows[0].user_id;
+        if (rows[0] === undefined || rows.length === 0 || rows === null || user_id === undefined) {
+            return false;
+        } else {
+            let { rows } = await pool.query('SELECT verified from users where user_id=$1', [user_id]);
+            if (rows[0] === undefined || rows.length[0] == 0 || rows == null) {
+                return false;
+            } else {
+                return rows[0].verified == 'Y' ? true : false;
+            }
+        }
+    } catch(e) {
+        return false;
+    }
+}
+
 
 /**
  * Encrypt any information with a cipher.
